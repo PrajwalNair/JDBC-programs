379c5466bd82322e63d86fe0198d7ecb3d490aec
unknown <you@example.com>
2022-10-25 10:47:01 +0530

    JDBC 23-10-2022

--- person/src/com/xworkz/person/repository/PersonRepositoryImpl.java
+++ person/src/com/xworkz/person/repository/PersonRepositoryImpl.java
@@ -1,6 +1,6 @@
 package com.xworkz.person.repository;
 
-import java.io.Reader;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -80,32 +80,33 @@ public class PersonRepositoryImpl implements PersonRepository{
 		try {
 			Class.forName(DRIVER.getValues());
 			Connection connection = DriverManager.getConnection(URL.getValues(), USERNAME.getValues(), SECRET.getValues());
-			String read = "select * from person where age >20 order by name";
+			String read = "select * from person where age >"+age+" order by name";
 			Statement createStatement = connection.createStatement();
 			ResultSet executeQuery = createStatement.executeQuery(read);
 			while (executeQuery.next()) {
-				int id = executeQuery.getInt(1);
+//				int id = executeQuery.getInt(1);
 				String name = executeQuery.getString(2);
-				String email = executeQuery.getString(3);
-				long mobileNumber = executeQuery.getLong(4);
-				String gender = executeQuery.getString(5);
-				String qualification = executeQuery.getString(6);
-				boolean married = executeQuery.getBoolean(7);
-				boolean working = executeQuery.getBoolean(8);
-				String companyName = executeQuery.getString(9);
-				int salary = executeQuery.getInt(10);
+//				String email = executeQuery.getString(3);
+//				long mobileNumber = executeQuery.getLong(4);
+//				String gender = executeQuery.getString(5);
+//				String qualification = executeQuery.getString(6);
+//				boolean married = executeQuery.getBoolean(7);
+//				boolean working = executeQuery.getBoolean(8);
+//				String companyName = executeQuery.getString(9);
+//				int salary = executeQuery.getInt(10);
 				int age1 = executeQuery.getInt(11);
-				double experience = executeQuery.getDouble(12);
-				String location = executeQuery.getString(13);
-				String city = executeQuery.getString(14);
-				String state = executeQuery.getString(15);
-				String country = executeQuery.getString(16);
-				long aadhaarNumber = executeQuery.getLong(17);
-				String panNumber = executeQuery.getString(18);
-				boolean alive = executeQuery.getBoolean(19);
-				long bankAccountNo = executeQuery.getLong(20);
+//				double experience = executeQuery.getDouble(12);
+//				String location = executeQuery.getString(13);
+//				String city = executeQuery.getString(14);
+//				String state = executeQuery.getString(15);
+//				String country = executeQuery.getString(16);
+//				long aadhaarNumber = executeQuery.getLong(17);
+//				String panNumber = executeQuery.getString(18);
+//				boolean alive = executeQuery.getBoolean(19);
+//				long bankAccountNo = executeQuery.getLong(20);
 				
-				System.out.println(id+" "+name+" "+email+" "+mobileNumber+" "+gender+" "+qualification+" "+married+" "+working+" "+companyName+" "+salary+" "+age1+" "+experience+" "+location+" "+city+" "+state+" "+country+" "+aadhaarNumber+" "+panNumber+" "+alive+" "+bankAccountNo);
+//				System.out.println(id+" "+name+" "+email+" "+mobileNumber+" "+gender+" "+qualification+" "+married+" "+working+" "+companyName+" "+salary+" "+age1+" "+experience+" "+location+" "+city+" "+state+" "+country+" "+aadhaarNumber+" "+panNumber+" "+alive+" "+bankAccountNo);
+				System.out.println(name+" "+age1);
 			}
 			
 			
@@ -124,32 +125,33 @@ public class PersonRepositoryImpl implements PersonRepository{
 		try {
 			Class.forName(DRIVER.getValues());
 			Connection connection = DriverManager.getConnection(URL.getValues(), USERNAME.getValues(), SECRET.getValues());
-			String read = "select * from person where gender = 'M' order by name desc";
+			String read = "select * from person where gender = '"+gender+"' order by name desc";
 			Statement createStatement = connection.createStatement();
 			ResultSet executeQuery = createStatement.executeQuery(read);
 			while (executeQuery.next()) {
-				int id = executeQuery.getInt(1);
+//				int id = executeQuery.getInt(1);
 				String name = executeQuery.getString(2);
-				String email = executeQuery.getString(3);
-				long mobileNumber = executeQuery.getLong(4);
+//				String email = executeQuery.getString(3);
+//				long mobileNumber = executeQuery.getLong(4);
 				String gender1 = executeQuery.getString(5);
-				String qualification = executeQuery.getString(6);
-				boolean married = executeQuery.getBoolean(7);
-				boolean working = executeQuery.getBoolean(8);
-				String companyName = executeQuery.getString(9);
-				int salary = executeQuery.getInt(10);
-				int age1 = executeQuery.getInt(11);
-				double experience = executeQuery.getDouble(12);
-				String location = executeQuery.getString(13);
-				String city = executeQuery.getString(14);
-				String state = executeQuery.getString(15);
-				String country = executeQuery.getString(16);
-				long aadhaarNumber = executeQuery.getLong(17);
-				String panNumber = executeQuery.getString(18);
-				boolean alive = executeQuery.getBoolean(19);
-				long bankAccountNo = executeQuery.getLong(20);
+//				String qualification = executeQuery.getString(6);
+//				boolean married = executeQuery.getBoolean(7);
+//				boolean working = executeQuery.getBoolean(8);
+//				String companyName = executeQuery.getString(9);
+//				int salary = executeQuery.getInt(10);
+//				int age1 = executeQuery.getInt(11);
+//				double experience = executeQuery.getDouble(12);
+//				String location = executeQuery.getString(13);
+//				String city = executeQuery.getString(14);
+//				String state = executeQuery.getString(15);
+//				String country = executeQuery.getString(16);
+//				long aadhaarNumber = executeQuery.getLong(17);
+//				String panNumber = executeQuery.getString(18);
+//				boolean alive = executeQuery.getBoolean(19);
+//				long bankAccountNo = executeQuery.getLong(20);
 				
-				System.out.println(id+" "+name+" "+email+" "+mobileNumber+" "+gender1+" "+qualification+" "+married+" "+working+" "+companyName+" "+salary+" "+age1+" "+experience+" "+location+" "+city+" "+state+" "+country+" "+aadhaarNumber+" "+panNumber+" "+alive+" "+bankAccountNo);
+//				System.out.println(id+" "+name+" "+email+" "+mobileNumber+" "+gender1+" "+qualification+" "+married+" "+working+" "+companyName+" "+salary+" "+age1+" "+experience+" "+location+" "+city+" "+state+" "+country+" "+aadhaarNumber+" "+panNumber+" "+alive+" "+bankAccountNo);
+				System.out.println(name+" "+gender1);
 			}
 			
 			
@@ -165,32 +167,33 @@ public class PersonRepositoryImpl implements PersonRepository{
 		try {
 			Class.forName(DRIVER.getValues());
 			Connection connection = DriverManager.getConnection(URL.getValues(), USERNAME.getValues(), SECRET.getValues());
-			String read = "select * from person where salary > 5000 order by salary desc";
+			String read = "select * from person where salary > "+salary+" order by salary desc";
 			Statement createStatement = connection.createStatement();
 			ResultSet executeQuery = createStatement.executeQuery(read);
 			while (executeQuery.next()) {
-				int id = executeQuery.getInt(1);
-				String name = executeQuery.getString(2);
-				String email = executeQuery.getString(3);
-				long mobileNumber = executeQuery.getLong(4);
-				String gender1 = executeQuery.getString(5);
-				String qualification = executeQuery.getString(6);
-				boolean married = executeQuery.getBoolean(7);
-				boolean working = executeQuery.getBoolean(8);
-				String companyName = executeQuery.getString(9);
+//				int id = executeQuery.getInt(1);
+//				String name = executeQuery.getString(2);
+//				String email = executeQuery.getString(3);
+//				long mobileNumber = executeQuery.getLong(4);
+//				String gender1 = executeQuery.getString(5);
+//				String qualification = executeQuery.getString(6);
+//				boolean married = executeQuery.getBoolean(7);
+//				boolean working = executeQuery.getBoolean(8);
+//				String companyName = executeQuery.getString(9);
 				int salary1 = executeQuery.getInt(10);
-				int age1 = executeQuery.getInt(11);
-				double experience = executeQuery.getDouble(12);
-				String location = executeQuery.getString(13);
-				String city = executeQuery.getString(14);
-				String state = executeQuery.getString(15);
-				String country = executeQuery.getString(16);
-				long aadhaarNumber = executeQuery.getLong(17);
-				String panNumber = executeQuery.getString(18);
-				boolean alive = executeQuery.getBoolean(19);
-				long bankAccountNo = executeQuery.getLong(20);
+//				int age1 = executeQuery.getInt(11);
+//				double experience = executeQuery.getDouble(12);
+//				String location = executeQuery.getString(13);
+//				String city = executeQuery.getString(14);
+//				String state = executeQuery.getString(15);
+//				String country = executeQuery.getString(16);
+//				long aadhaarNumber = executeQuery.getLong(17);
+//				String panNumber = executeQuery.getString(18);
+//				boolean alive = executeQuery.getBoolean(19);
+//				long bankAccountNo = executeQuery.getLong(20);
 				
-				System.out.println(id+" "+name+" "+email+" "+mobileNumber+" "+gender1+" "+qualification+" "+married+" "+working+" "+companyName+" "+salary1+" "+age1+" "+experience+" "+location+" "+city+" "+state+" "+country+" "+aadhaarNumber+" "+panNumber+" "+alive+" "+bankAccountNo);
+				//System.out.println(id+" "+name+" "+email+" "+mobileNumber+" "+gender1+" "+qualification+" "+married+" "+working+" "+companyName+" "+salary1+" "+age1+" "+experience+" "+location+" "+city+" "+state+" "+country+" "+aadhaarNumber+" "+panNumber+" "+alive+" "+bankAccountNo);
+				System.out.println(salary1);
 			}
 			
 			
